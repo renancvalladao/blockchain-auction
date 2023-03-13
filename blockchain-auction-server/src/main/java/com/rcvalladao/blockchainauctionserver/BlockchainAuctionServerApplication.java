d4720cbbed85261f488dfbf261da9d3d0c813761
Renan Valladão <renancvalladao@gmail.com>
2023-03-13 15:40:46 -0300

    Initializing server project

---
+++ blockchain-auction-server/src/main/java/com/rcvalladao/blockchainauctionserver/BlockchainAuctionServerApplication.java
@@ -0,0 +1,13 @@
+package com.rcvalladao.blockchainauctionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainAuctionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainAuctionServerApplication.class, args);
+    }
+
+}
---
+++ blockchain-auction-server/src/test/java/com/rcvalladao/blockchainauctionserver/BlockchainAuctionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcvalladao.blockchainauctionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainAuctionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
