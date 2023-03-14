8b753b95fec87887a5058cbb4a1cf11402aa075d
Renan Valladão <renancvalladao@gmail.com>
2023-03-14 17:28:07 -0300

    Adding bidding and finish auction logic

--- blockchain-auction-server/src/main/resources/contracts/Auction.sol
+++ blockchain-auction-server/src/main/resources/contracts/Auction.sol
@@ -10,13 +10,40 @@ contract Auction {
     }
 
     Requirements requirements;
+    address owner;
+    address lowestBidder;
+    uint lowestBid;
+    uint endTime;
+    bool ended;
 
-    constructor(Requirements memory _requirements) {
+    constructor(Requirements memory _requirements, uint biddingTime) {
         requirements = _requirements;
+        owner = msg.sender;
+        endTime = block.timestamp + biddingTime;
     }
 
     function getRequirements() public view returns (Requirements memory) {
         return requirements;
+    }
+
+    function getWinner() public view returns (address) {
+        require(ended, "Auction has not ended yet");
+        return lowestBidder;
+    }
+
+    function placeBid(uint value) external {
+        require(!ended, "Auction has already ended");
+        require(value > 0, "Bid value must be greater than zero");
+        if (value < lowestBid || lowestBid == 0) {
+            lowestBid = value;
+            lowestBidder = msg.sender;
+        }
+    }
+
+    function finishAuction() external {
+        require(msg.sender == owner, "Only the owner can finish the auction");
+        require(block.timestamp >= endTime, "Auction has not ended yet");
+        ended = true;
     }
 
 }
