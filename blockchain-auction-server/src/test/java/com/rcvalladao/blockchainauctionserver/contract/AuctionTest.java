6cbbe49d257fbcf5e8976c987d711b071b79b003
Renan Valladão <renancvalladao@gmail.com>
2023-03-15 09:32:17 -0300

    Testing get winner and auction logic

--- blockchain-auction-server/src/test/java/com/rcvalladao/blockchainauctionserver/contract/AuctionTest.java
+++ blockchain-auction-server/src/test/java/com/rcvalladao/blockchainauctionserver/contract/AuctionTest.java
@@ -7,6 +7,7 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.exceptions.TransactionException;
 import org.web3j.tx.RawTransactionManager;
 import org.web3j.tx.TransactionManager;
+import org.web3j.tx.exceptions.ContractCallException;
 import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.utils.Convert;
 
@@ -81,6 +82,45 @@ class AuctionTest {
         Auction auction = Auction.deploy(web3j, transactionManager, gasProvider, requirements, BigInteger.TEN).send();
 
         assertThrows(TransactionException.class, () -> auction.placeBid(BigInteger.ZERO).send());
+    }
+
+    @Test
+    void givenOneBid_whenGetWinner_thenReturnWinner(Web3j web3j, TransactionManager transactionManager, ContractGasProvider gasProvider) throws Exception {
+        Auction.Requirements requirements = new Auction.Requirements("Name", "Type", BigInteger.TWO);
+        Auction auction = Auction.deploy(web3j, transactionManager, gasProvider, requirements, BigInteger.ZERO).send();
+        auction.placeBid(BigInteger.TEN).send();
+        auction.finishAuction().send();
+        String winnerAddress = auction.getWinner().send();
+
+        assertEquals(transactionManager.getFromAddress(), winnerAddress);
+    }
+
+    @Test
+    void givenTwoBids_whenGetWinner_thenReturnWinner(Web3j web3j, TransactionManager transactionManager, ContractGasProvider gasProvider) throws Exception {
+        Auction.Requirements requirements = new Auction.Requirements("Name", "Type", BigInteger.TWO);
+        Auction auction = Auction.deploy(web3j, transactionManager, gasProvider, requirements, BigInteger.ZERO).send();
+        auction.placeBid(BigInteger.TEN).send();
+
+        // Using another account
+        Credentials notOwner = Credentials.create("a");
+        transactionManager.sendTransaction(gasProvider.getGasPrice(""), gasProvider.getGasLimit(""),
+                notOwner.getAddress(), "", Convert.toWei("1.0", Convert.Unit.ETHER).toBigInteger());
+        TransactionManager myTransactionManager = new RawTransactionManager(web3j, notOwner);
+        Auction notOwnerAuction = Auction.load(auction.getContractAddress(), web3j, myTransactionManager, gasProvider);
+        notOwnerAuction.placeBid(BigInteger.TWO).send();
+
+        auction.finishAuction().send();
+        String winnerAddress = auction.getWinner().send();
+
+        assertEquals(notOwner.getAddress(), winnerAddress);
+    }
+
+    @Test
+    void givenAuctionNotEnded_whenGetWinner_thenReturnError(Web3j web3j, TransactionManager transactionManager, ContractGasProvider gasProvider) throws Exception {
+        Auction.Requirements requirements = new Auction.Requirements("Name", "Type", BigInteger.TWO);
+        Auction auction = Auction.deploy(web3j, transactionManager, gasProvider, requirements, BigInteger.TEN).send();
+
+        assertThrows(ContractCallException.class, () -> auction.getWinner().send());
     }
 
 }
