e0a5275ad4a838405ae5369da55a6079fdd47278
Renan Valladão <renancvalladao@gmail.com>
2023-03-15 10:49:22 -0300

    Initializing vnf provider bid service project

---
+++ blockchain-auction-bid-service/src/main/java/com/rcvalladao/blockchainauctionbidservice/BlockchainAuctionBidServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rcvalladao.blockchainauctionbidservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainAuctionBidServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainAuctionBidServiceApplication.class, args);
+	}
+
+}
---
+++ blockchain-auction-bid-service/src/test/java/com/rcvalladao/blockchainauctionbidservice/BlockchainAuctionBidServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcvalladao.blockchainauctionbidservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainAuctionBidServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
