dd394de12798e6525a25d4f94181ea29c2d1c985
Renan Valladão <renancvalladao@gmail.com>
2023-03-20 13:51:19 -0300

    Adding GET providers endpoint

--- blockchain-auction-server/src/main/java/com/rcvalladao/blockchainauctionserver/controller/ProviderController.java
+++ blockchain-auction-server/src/main/java/com/rcvalladao/blockchainauctionserver/controller/ProviderController.java
@@ -6,9 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -16,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProviderController {
 
     private final ProviderService providerService;
+
+    @GetMapping("/providers")
+    public List<ProviderInfo> getProviders() {
+        return this.providerService.getProvidersInfo();
+    }
 
     @PostMapping("/providers")
     public ResponseEntity<Object> addProvider(@RequestBody ProviderInfo providerInfo) {
