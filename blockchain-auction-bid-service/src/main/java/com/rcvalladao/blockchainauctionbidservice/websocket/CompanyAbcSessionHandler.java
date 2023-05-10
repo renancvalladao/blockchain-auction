c35ecee1741702400fa0f99c98e636a6e759acfc
Renan Valladão <renancvalladao@gmail.com>
2023-05-10 11:57:42 -0300

    Adding websockets new classes

---
+++ blockchain-auction-bid-service/src/main/java/com/rcvalladao/blockchainauctionbidservice/websocket/CompanyAbcSessionHandler.java
@@ -0,0 +1,44 @@
+package com.rcvalladao.blockchainauctionbidservice.websocket;
+
+import com.rcvalladao.blockchainauctionbidservice.service.CompanyAbcBidService;
+import com.rcvalladao.blockchainauctionserver.dto.ContractInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class CompanyAbcSessionHandler extends StompSessionHandlerAdapter {
+
+    private final CompanyAbcBidService companyAbcBidService;
+
+    @Override
+    public void afterConnected(StompSession session, @NotNull StompHeaders connectedHeaders) {
+        session.subscribe("/auction-notifier", this);
+    }
+
+    @NotNull
+    @Override
+    public Type getPayloadType(@NotNull StompHeaders headers) {
+        return ContractInfo.class;
+    }
+
+    @Override
+    public void handleFrame(@NotNull StompHeaders headers, Object payload) {
+        ContractInfo contractInfo = (ContractInfo) payload;
+        log.info("New auction at {}", contractInfo.getAddress());
+        try {
+            this.companyAbcBidService.placeBid(contractInfo);
+        } catch (Exception e) {
+            log.error("Could not place bid on contract at {}", contractInfo.getAddress(), e);
+        }
+    }
+
+}
---
+++ blockchain-auction-bid-service/src/main/java/com/rcvalladao/blockchainauctionbidservice/websocket/CompanyDefWebSocketClient.java
@@ -0,0 +1,40 @@
+package com.rcvalladao.blockchainauctionbidservice.websocket;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHttpHeaders;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.web3j.crypto.Credentials;
+
+@Component
+public class CompanyDefWebSocketClient {
+
+    private final CompanyDefSessionHandler companyDefSessionHandler;
+    private final String serverWebsocketUrl;
+    private final String privateKey;
+
+    public CompanyDefWebSocketClient(CompanyDefSessionHandler companyDefSessionHandler,
+                                     @Value("${auctionServer.websocket}") String serverWebsocketUrl,
+                                     @Value("${companyDef.privateKey}") String privateKey) {
+        this.companyDefSessionHandler = companyDefSessionHandler;
+        this.serverWebsocketUrl = serverWebsocketUrl;
+        this.privateKey = privateKey;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        StandardWebSocketClient webSocketClient = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+        Credentials credentials = Credentials.create(this.privateKey);
+        headers.add("address", credentials.getAddress());
+        headers.add("name", "Company DEF");
+        stompClient.connectAsync(this.serverWebsocketUrl, headers, this.companyDefSessionHandler);
+    }
+
+}
