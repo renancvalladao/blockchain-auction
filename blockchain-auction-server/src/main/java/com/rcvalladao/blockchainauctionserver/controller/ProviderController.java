491bb603ca5fd5fcb745736e80331e866189851e
Renan Valladão <renancvalladao@gmail.com>
2023-05-10 11:51:20 -0300

    Removing add provider endpoint

--- blockchain-auction-server/src/main/java/com/rcvalladao/blockchainauctionserver/controller/ProviderController.java
+++ blockchain-auction-server/src/main/java/com/rcvalladao/blockchainauctionserver/controller/ProviderController.java
@@ -4,11 +4,7 @@ import com.rcvalladao.blockchainauctionserver.dto.ProviderInfo;
 import com.rcvalladao.blockchainauctionserver.service.ProviderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,13 +19,6 @@ public class ProviderController {
     @GetMapping("/providers")
     public List<ProviderInfo> getProviders() {
         return this.providerService.getProvidersInfo();
-    }
-
-    @PostMapping("/providers")
-    public ResponseEntity<Object> addProvider(@RequestBody ProviderInfo providerInfo) {
-        log.info("New add provider request");
-        this.providerService.addProvider(providerInfo);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
