b2ba7ac4eecb8209e5a6b4983c86b6fbc9670d34
Renan Valladão <renancvalladao@gmail.com>
2023-05-10 11:50:40 -0300

    Changing providers list to map

--- blockchain-auction-server/src/main/java/com/rcvalladao/blockchainauctionserver/service/ProviderService.java
+++ blockchain-auction-server/src/main/java/com/rcvalladao/blockchainauctionserver/service/ProviderService.java
@@ -5,23 +5,28 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class ProviderService {
 
-    private final List<ProviderInfo> providersInfo = new ArrayList<>();
+    private final Map<String, ProviderInfo> providersInfo = new HashMap<>();
 
     public void addProvider(ProviderInfo providerInfo) {
-        this.providersInfo.add(providerInfo);
-        log.info("New provider " + providerInfo.getName() + " endpoint registered");
+        this.providersInfo.put(providerInfo.getAddress(), providerInfo);
+        log.info("New provider " + providerInfo.getName() + " registered");
     }
 
     public List<ProviderInfo> getProvidersInfo() {
-        return this.providersInfo;
+        return this.providersInfo.values().stream().toList();
+    }
+
+    public ProviderInfo getProviderInfoByAddress(String address) {
+        return this.providersInfo.get(address);
     }
 
 }
