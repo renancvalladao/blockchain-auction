bc3887b833844ed534f73639375ee9f06ecf3c04
Renan Valladão <renancvalladao@gmail.com>
2023-05-25 09:45:52 -0300

    Using concurrent map

--- blockchain-auction-server/src/main/java/com/rcvalladao/blockchainauctionserver/service/ProviderService.java
+++ blockchain-auction-server/src/main/java/com/rcvalladao/blockchainauctionserver/service/ProviderService.java
@@ -5,16 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class ProviderService {
 
-    private final Map<String, ProviderInfo> providersInfo = new HashMap<>();
+    private final Map<String, ProviderInfo> providersInfo = new ConcurrentHashMap<>();
 
     public void addProvider(ProviderInfo providerInfo) {
         this.providersInfo.put(providerInfo.getAddress(), providerInfo);
