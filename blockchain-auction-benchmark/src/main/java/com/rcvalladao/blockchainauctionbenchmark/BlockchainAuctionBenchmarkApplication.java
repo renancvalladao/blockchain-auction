e969c7507c67dd435e66aee91900a70262999bc4
Renan Valladão <renancvalladao@gmail.com>
2023-05-25 09:27:02 -0300

    Creating benchmark project

---
+++ blockchain-auction-benchmark/src/main/java/com/rcvalladao/blockchainauctionbenchmark/BlockchainAuctionBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package com.rcvalladao.blockchainauctionbenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainAuctionBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainAuctionBenchmarkApplication.class, args);
+	}
+
+}
